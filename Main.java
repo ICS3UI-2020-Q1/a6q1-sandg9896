40a3107dce95a7653ad02aa76694dfd79b55a1b1
sandg9896 <sandg9896@wrdsb.ca>
2020-10-08 13:40:21 +0000

    Heights

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
+import java.util.Scanner;
 /**
  *
- * @author 
+ * @ Gavin
  */
 public class Main {
 
@@ -9,7 +10,41 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
-    
+    // Create scanner for user input
+    Scanner input = new Scanner(System.in);
+
+    // ask user how many heights they would like to enter
+    System.out.println("How many heights would you like to enter?");
+    int heights = input.nextInt();
+
+    // create array with 5 spots
+    double[] total = new double[heights];
+   
+    // put heights into array using loop
+    for(int i = 0; i < total.length; i++){
+     System.out.println("Please enter a height using cm");
+     total[i] = input.nextDouble();
+    }
+    // create a variable to store the sum
+    double sum = 0;
+
+    // use a for loop to go trhough the array and add sum
+    for(int i = 0; i < total.length; i++){
+      // add total to sum
+      sum = sum + total[i];
+    }
+
+    // detirmine the average
+    double average = sum / total.length;
+
+    // let user know
+    System.out.println("The avereage of the heights entered is " + average + "cm");
+   
+    // tell user the heights above the average
+    System.out.println("The heights above average are: ");
+    for(int i = 0; i < total.length; i++){
+      if(total[i] > average)
+      System.out.println(total[i]);
+    } 
   }
 }
